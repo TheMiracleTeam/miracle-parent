4527296499c84eabc60b4cf82d47ab27eefa9497
AprLieZ <289512298@qq.com>
2018-09-26 17:19:34 +0800

    [ADD]DigestUtil工具类新增md5加密、验证加密

--- miracle-common/src/main/java/com/miracle/common/util/DigestUtil.java
+++ miracle-common/src/main/java/com/miracle/common/util/DigestUtil.java
@@ -24,4 +24,36 @@ public class DigestUtil extends DigestUtils {
         Arrays.sort(array);
         return signature.equals(DigestUtils.sha1Hex(array[0] + array[1] + array[2]));
     }
+
+    /**
+     * md5加密
+     * @param msg 原字符串
+     * @param key 中间位置插入key
+     * @return String 加密结果
+     */
+    public static String md5(String msg, String key) {
+        if(msg == null || msg == "") {
+            return "";
+        }
+        String sb = new StringBuffer(msg).insert(msg.length()/2, key).toString();
+        String md5=DigestUtils.md5Hex(sb);
+        return md5;
+    }
+
+    /**
+     * 验证md5
+     * @param msg 待验证字符串
+     * @param key 中间key
+     * @param md5 md5字符串
+     * @return boolean 成功或否
+     */
+    public static boolean verifyMD5(String msg, String key, String md5) {
+        //根据传入的密钥进行验证
+        String md5Text = md5(msg, key);
+        if(md5Text.equalsIgnoreCase(md5))
+        {
+            return true;
+        }
+        return false;
+    }
 }
